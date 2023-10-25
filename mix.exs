fb8a04773dd9c939603b692f8ce94cb53331cfe9
mustela <ejankowski@gmail.com>
2023-10-25 15:30:51 +0200

    Fixing actions

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule Pullify.MixProject do
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
       aliases: aliases(),
       deps: deps(),
 
